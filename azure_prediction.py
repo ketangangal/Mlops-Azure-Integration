c274f487d4df9b0167b720e986b13256f069654e
ketanGangal <ketangangal98@gmail.com>
2022-03-11 21:55:07 +0530

    prediction file added

--- azure_prediction.py
+++ azure_prediction.py
@@ -17,7 +17,10 @@ if __name__ == "__main__":
     config = read_config(path)
     scoring_uri = config['azure_endpoint']['scoring_uri']
 
-    data = pd.read_json('{"fixed acidity":{"0":7.4},"volatile acidity":{"0":0.7},"citric acid":{"0":0},"residual sugar":{"0":1.9},"chlorides":{"0":0.076},"free sulfur dioxide":{"0":11},"total sulfur dioxide":{"0":34},"density":{"0":0.9978},"pH":{"0":3.51},"sulphates":{"0":0.56},"alcohol":{"0":9.4}}')
+    data = pd.read_json('{"fixed acidity":{"0":7.4},"volatile acidity":{"0":0.7},"citric acid":{"0":0},"residual '
+                        'sugar":{"0":1.9},"chlorides":{"0":0.076},"free sulfur dioxide":{"0":11},"total sulfur '
+                        'dioxide":{"0":34},"density":{"0":0.9978},"pH":{"0":3.51},"sulphates":{"0":0.56},"alcohol":{'
+                        '"0":9.4}}')
     input_json = data.to_json(orient='split')
 
     response = query(scoring_uri, input_json)
