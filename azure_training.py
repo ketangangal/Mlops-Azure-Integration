704259ae78a5420a32b80833ff4a3ca8e9f630f1
ketanGangal <ketangangal98@gmail.com>
2022-03-12 18:07:47 +0530

    files

--- azure_training.py
+++ azure_training.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.7
-
+    #mlflow.set_tracking_uri("http://127.0.0.1:5000")
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
---
+++ test.py
@@ -0,0 +1,2 @@
+#mlflow server --backend-store-uri mysql://admin:password@mlops-azure.mysql.database.azure.com/DB-name
+# --default-artifact-root ./artifacts --host 127.0.0.1 -p 5000
